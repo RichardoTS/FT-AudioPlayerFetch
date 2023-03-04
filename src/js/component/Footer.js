6c2561bc75e7566047667b57a286a72b71970b33
RichardoTS <ri.tapiasaez@gmail.com>
2023-03-04 20:25:38 +0000

    New Repo

---
+++ src/js/component/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        
+        <div className="text-white bg-dark p-5 text-center mt-5" id="ftr">
+            <button className="btn btn-secondary">Rewinds</button>
+            <button className="btn btn-secondary">Play</button>
+            <button className="btn btn-secondary">Forward</button>
+            
+        </div>
+    )
+}
+
+export default Footer;
