058fd5eceab1fc71b3daed88b3d8a4401dde0844
RichardoTS <ri.tapiasaez@gmail.com>
2023-03-06 21:05:10 +0000

    Init project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 //include images into your bundle
-import Footer from "./Footer.js";
+
 
 //create your first component
 const Home = () => {
 	return (
-		// <Music />
-		// <Reproductor />
-		<Footer />
+		<div className="text-center">asdf</div>
+		
 	);
 };
 
